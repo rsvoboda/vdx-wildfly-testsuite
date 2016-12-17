661e3b50a386c4de7ce43fcc6ced44259b7b5f3c
Rostislav Svoboda <rsvoboda@redhat.com>
2016-12-17 20:54:03 +0100

    host.xml - more simple tests

--- src/test/java/org/wildfly/test/integration/vdx/domain/HostXmlSmokeTestCase.java
+++ src/test/java/org/wildfly/test/integration/vdx/domain/HostXmlSmokeTestCase.java
@@ -22,8 +22,14 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.wildfly.extras.creaper.commands.foundation.offline.xml.GroovyXmlTransform;
+import org.wildfly.extras.creaper.commands.foundation.offline.xml.Subtree;
+import org.wildfly.extras.creaper.core.offline.OfflineCommand;
+import org.wildfly.extras.creaper.core.offline.OfflineCommandContext;
 import org.wildfly.test.integration.vdx.TestBase;
 import org.wildfly.test.integration.vdx.category.DomainTests;
+import org.wildfly.test.integration.vdx.transformations.AddNonExistentElementToMessagingSubsystem;
+import org.wildfly.test.integration.vdx.transformations.DoNothing;
 import org.wildfly.test.integration.vdx.utils.server.ServerConfig;
 
 import static org.junit.Assert.assertTrue;
@@ -39,8 +45,54 @@ import static org.junit.Assert.assertTrue;
 public class HostXmlSmokeTestCase extends TestBase {
 
     @Test
-    @ServerConfig(configuration = "host.xml", xmlTransformationGroovy = "host/AddUnexpectedElement.groovy")
-    public void modifyWsdlAddressElementWithNoValue()throws Exception {
+    @ServerConfig(configuration = "host.xml", xmlTransformationGroovy = "host/ManagementAuditLogElement.groovy")
+    public void addUnexpectedExtension() throws Exception {
         container().tryStartAndWaitForFail();
     }
+
+    private void addElementAndStart(Subtree subtree, String elementXml) throws Exception {
+        container().tryStartAndWaitForFail(
+                (OfflineCommand) ctx -> ctx.client.apply(GroovyXmlTransform.of(DoNothing.class, "AddElement.groovy")
+                        .subtree("path", subtree).parameter("elementXml", elementXml)
+                        .build()));
+    }
+
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInJvms() throws Exception {
+        addElementAndStart( Subtree.jvms(), "<element>value</element>\n");
+    }
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInServers() throws Exception {
+        addElementAndStart( Subtree.servers(), "<element>value</element>\n");
+    }
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInDC() throws Exception {
+        addElementAndStart( Subtree.domainController(), "<element>value</element>\n");
+    }
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInInterfaces() throws Exception {
+        addElementAndStart( Subtree.interfaces(), "<element>value</element>\n");
+    }
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInManagement() throws Exception {
+        addElementAndStart( Subtree.management(), "<element>value</element>\n");
+    }
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInExtensions() throws Exception {
+        addElementAndStart( Subtree.extensions(), "<element>value</element>\n");
+    }
+    @Test
+    @ServerConfig(configuration = "host.xml")
+    public void appendElementInProperties() throws Exception {
+        addElementAndStart( Subtree.systemProperties(), "<element>value</element>\n");
+    }
+
+
+
 }
